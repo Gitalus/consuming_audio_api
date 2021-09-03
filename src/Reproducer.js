e67944f778bf203f893fb2dd78ba205c79230004
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-03 18:21:33 -0400

    complete behavior of buttons

--- src/Reproducer.js
+++ src/Reproducer.js
@@ -1,51 +1,84 @@
-import React, { useState, useRef } from 'react';
-import { Song } from './components/Song';
+import React, { useState, useRef, useEffect } from 'react';
 
 export const Reproducer = () => {
-    const [songs, ] = useState([
+    const [songs] = useState([
         { "id":1, "category":"game", "name":"Mario Castle", "url":"files/mario/songs/castle.mp3" },
         { "id":2, "category":"game", "name":"Mario Star", "url":"files/mario/songs/hurry-starman.mp3"},
         { "id":3, "category":"game", "name":"Mario Overworld", "url":"files/mario/songs/overworld.mp3"}
     ]);
-    const [selected, setSelected] = useState(songs[0].url);
+    const [current, setCurrent] = useState(0);
     const [playing, setPlaying] = useState(false);
 
     const audio = useRef();
+    
+    useEffect(() => {
+        audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[current].url}`;
+    }, [])
 
-    function selectPlay(data) {
-        if (selected === data.url && playing) {
+    function selectPlay(idx) {
+        if (current === idx && playing) {
             return;
-        } else {
-            audio.current.src = `https://assets.breatheco.de/apis/sound/${data.url}`;
-            setSelected(data.url);
+        }
+        audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[idx].url}`;
+        audio.current.play();
+        setCurrent(idx);
+        setPlaying(true);
+    }
+
+    function nextSong() {
+        if (current === songs.length - 1) {
+            audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[0].url}`;
             audio.current.play();
             setPlaying(true);
+            setCurrent(0);
+        } else {
+            audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[current + 1].url}`;
+            audio.current.play();
+            setPlaying(true);
+            setCurrent(current + 1);
         }
     }
     
+    function prevSong() {
+        if (current === 0) {
+            audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[songs.length - 1].url}`;
+            audio.current.play();
+            setPlaying(true);
+            setCurrent(songs.length - 1);
+        } else {
+            audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[current - 1].url}`;
+            audio.current.play();
+            setPlaying(true);
+            setCurrent(current - 1);
+        }
+    }
+
     function togglePlayPause() {
-        setPlaying(!playing);
-        playing ? audio.current.pause() : audio.current.play();
+        if (current !== null) {
+            setPlaying(!playing);
+            playing ? audio.current.pause() : audio.current.play();
+        }
     }
 
     return (
         <div className="container">
             <div className="list-songs">
                 {
-                    songs.map( song => {
-                        return <Song key={ song.id } data={ song } handlePlay={ selectPlay }/>
+                    songs.map( ( song, idx ) => {
+                        return <div className={"songs" + (playing && current === idx ? " active" : "")} onClick={ () => selectPlay(idx)}>
+                                    <span className="nSong">{idx + 1}</span>{ song.name }
+                                </div>
                     })
                 }
             </div>
             <footer>
-                <i className="fas fa-step-backward controls"></i>
+                <i className="fas fa-step-backward controls" onClick={ () => prevSong() }></i>
                 {
                     playing ? 
-                    <i className="fas fa-pause-circle play-control" onClick={ () => togglePlayPause() }></i>
-                    :
-                    <i className="fas fa-play-circle play-control" onClick={ () => togglePlayPause() }></i>
+                    (<i className="fas fa-pause-circle play-control" onClick={ () => togglePlayPause() }></i>
+                    ):(<i className="fas fa-play-circle play-control" onClick={ () => togglePlayPause() }></i>)
                 }
-                <i className="fas fa-step-forward controls"></i>
+                <i className="fas fa-step-forward controls" onClick={ () => nextSong() }></i>
                 <audio ref={ audio } hidden></audio>
             </footer>
         </div>
