0ae1dc02c99643cfe8fb5ad19eb0df87961773cb
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-05 12:53:07 -0300

    complete progressBar with functionality

--- src/Reproducer.js
+++ src/Reproducer.js
@@ -7,27 +7,56 @@ export const Reproducer = () => {
     const [playing, setPlaying] = useState(false);
     const [repeat, setRepeat] = useState(false);
     const [shuffle, setShuffle] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0)
 
     const songs = useFetch(); // Promise to fetch the song list, null by default
     
-    const audio = useRef(); // audio-player ref
-
-    useEffect(() => {
-        if (songs) {
-            audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[0].url}`;
-        }
-    }, [songs])
-
+    const audioPlayer = useRef();
+    const progressBar = useRef();
+    const animationRef = useRef(); // reference animation of thumb in progressBar
+    
     const { selectPlay, nextSong, prevSong, togglePlayPause } = handleMusic({ 
         songs, 
         current, 
         setCurrent,
         playing, 
         setPlaying, 
-        audio, 
+        audioPlayer, 
         repeat, 
-        shuffle 
+        shuffle,
+        animationRef,
+        whilePlaying
     });
+
+    useEffect(() => {
+        if (songs) {
+            audioPlayer.current.src = `https://assets.breatheco.de/apis/sound/${songs[0].url}`;
+            audioPlayer.current.currentTime = currentTime;
+            changePlayerCurrentTime();
+        }
+    }, [songs])
+
+    useEffect(() => {
+        const seconds = Math.floor(audioPlayer.current.duration);
+        progressBar.current.max = seconds;
+    }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState])
+
+
+    function whilePlaying() {
+        progressBar.current.value = audioPlayer.current.currentTime;
+        changePlayerCurrentTime();
+        animationRef.current = requestAnimationFrame(whilePlaying);
+    }
+
+    function changeRange() {
+        audioPlayer.current.currentTime = progressBar.current.value;
+        changePlayerCurrentTime();
+    }
+
+    function changePlayerCurrentTime() {
+        progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / progressBar.current.max * 100}%`);
+        setCurrentTime(progressBar.current.value);
+    }
 
     return (
         <div className="container">
@@ -35,7 +64,7 @@ export const Reproducer = () => {
                 {
                     !!songs &&
                     songs.map( ( song, idx ) => {
-                        return <div key={idx} className={"songs" + (playing && current === idx ? " active" : "") + ( audio.current?.currentTime !== 0 && current === idx && !playing ? " paused" : "")} onClick={ () => selectPlay(idx)}>
+                        return <div key={idx} className={"songs" + (playing && current === idx ? " active" : "") + ( audioPlayer.current?.currentTime !== 0 && current === idx && !playing ? " paused" : "")} onClick={ () => selectPlay(idx)}>
                                     <span className="nSong">{idx + 1}</span>{ song.name }
                                 </div>
                     })
@@ -43,7 +72,7 @@ export const Reproducer = () => {
             </div>
             <footer>
                 <div className="progress-container">
-                    <input type="range" className="progress-bar" />
+                    <input type="range" className="progress-bar" defaultValue="0" ref={ progressBar } onChange={ changeRange } />
                 </div>
                 <i className={"far fa-random controls" + (shuffle ? " active-icon" : "")} onClick={ () => {
                                                                                                             setShuffle(!shuffle);
@@ -57,9 +86,9 @@ export const Reproducer = () => {
                 <i className="fas fa-step-forward controls" onClick={ nextSong }></i>
                 <i className={"far fa-repeat-alt controls" + (repeat ? " active-icon" : "")} onClick={ () => {
                                                                                                             setRepeat(!repeat);
-                                                                                                            audio.current.loop = !audio.current.loop;
+                                                                                                            audioPlayer.current.loop = !audioPlayer.current.loop;
                                                                                                         }}></i>
-                <audio ref={ audio } hidden onEnded={() => nextSong() }></audio>
+                <audio ref={ audioPlayer } hidden onEnded={() => nextSong() }></audio>
                 <i className="fal fa-volume-up"></i>
             </footer>
         </div>
--- src/helper.js
+++ src/helper.js
@@ -1,13 +1,14 @@
 
 
-export function handleMusic({ songs, current, setCurrent, playing, setPlaying, audio, repeat, shuffle }) {
+export function handleMusic({ songs, current, setCurrent, playing, setPlaying, audioPlayer, repeat, shuffle, animationRef, whilePlaying }) {
 
     function selectPlay(idx) {
         if (current === idx && playing) {
             return;
         }
-        audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[idx].url}`;
-        audio.current.play();
+        audioPlayer.current.src = `https://assets.breatheco.de/apis/sound/${songs[idx].url}`;
+        audioPlayer.current.play();
+        animationRef.current = requestAnimationFrame(whilePlaying);
         setCurrent(idx);
         setPlaying(true);
     }
@@ -18,13 +19,15 @@ export function handleMusic({ songs, current, setCurrent, playing, setPlaying, a
             shuffleSong();
         }
         else if (current === songs.length - 1) {
-            audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[0].url}`;
-            audio.current.play();
+            audioPlayer.current.src = `https://assets.breatheco.de/apis/sound/${songs[0].url}`;
+            audioPlayer.current.play();
+            animationRef.current = requestAnimationFrame(whilePlaying);
             setPlaying(true);
             setCurrent(0);
         } else {
-            audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[current + 1].url}`;
-            audio.current.play();
+            audioPlayer.current.src = `https://assets.breatheco.de/apis/sound/${songs[current + 1].url}`;
+            audioPlayer.current.play();
+            animationRef.current = requestAnimationFrame(whilePlaying);
             setPlaying(true);
             setCurrent(current + 1);
         }
@@ -35,29 +38,38 @@ export function handleMusic({ songs, current, setCurrent, playing, setPlaying, a
             shuffleSong();
         }
         else if (current === 0) {
-            audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[songs.length - 1].url}`;
-            audio.current.play();
+            audioPlayer.current.src = `https://assets.breatheco.de/apis/sound/${songs[songs.length - 1].url}`;
+            audioPlayer.current.play();
+            animationRef.current = requestAnimationFrame(whilePlaying);
             setPlaying(true);
             setCurrent(songs.length - 1);
         } else {
-            audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[current - 1].url}`;
-            audio.current.play();
+            audioPlayer.current.src = `https://assets.breatheco.de/apis/sound/${songs[current - 1].url}`;
+            audioPlayer.current.play();
+            animationRef.current = requestAnimationFrame(whilePlaying);
             setPlaying(true);
             setCurrent(current - 1);
         }
     }
 
     function togglePlayPause() {
-        if (songs !== null) {
-            setPlaying(!playing);
-            playing ? audio.current.pause() : audio.current.play();
+        if (songs === null) return;
+        setPlaying(!playing);
+        if (playing) {
+            audioPlayer.current.pause(); 
+            cancelAnimationFrame(animationRef.current);
+        } else {
+            audioPlayer.current.play();
+            animationRef.current = requestAnimationFrame(whilePlaying);
         }
     }
 
     function shuffleSong() {
         let randomValue = Math.floor(Math.random() * songs.length);
-        audio.current.src = `https://assets.breatheco.de/apis/sound/${songs[randomValue].url}`;
-        audio.current.play();
+        audioPlayer.current.src = `https://assets.breatheco.de/apis/sound/${songs[randomValue].url}`;
+        animationRef.current = requestAnimationFrame(whilePlaying);
+        audioPlayer.current.play();
+        setPlaying(true);
         setCurrent(randomValue);
     }
 
